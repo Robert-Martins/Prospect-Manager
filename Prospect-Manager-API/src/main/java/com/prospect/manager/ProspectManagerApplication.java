c07466df9f2416d947c10c3aeb17d58677008284
Robert-Martins <robertcantares@gmail.com>
2023-09-25 20:46:35 -0300

    feat/init-angular-and-spring-applications

---
+++ Prospect-Manager-API/src/main/java/com/prospect/manager/ProspectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.prospect.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectManagerApplication.class, args);
+	}
+
+}
