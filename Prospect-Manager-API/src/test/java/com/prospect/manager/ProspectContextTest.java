e9020231368d0d1d377811e9c4a0a11e2602b2d4
Robert-Martins <robertcantares@gmail.com>
2023-09-29 23:33:00 -0300

    feat/enhance-testing

--- Prospect-Manager-API/src/test/java/com/prospect/manager/ProspectContextTest.java
+++ Prospect-Manager-API/src/test/java/com/prospect/manager/ProspectContextTest.java
@@ -7,12 +7,11 @@ import com.prospect.manager.presentation.dtos.PersonDto;
 import com.prospect.manager.presentation.dtos.ProspectDto;
 import com.prospect.manager.presentation.services.IProspectService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.times;
@@ -20,8 +19,8 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(ProspectController.class)
+@AutoConfigureDataMongo
 public class ProspectContextTest {
 
     @Autowired
