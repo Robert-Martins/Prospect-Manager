72511ae06f644ef21bc713ad18088f7f203ab537
Robert-Martins <robertcantares@gmail.com>
2023-09-29 23:43:51 -0300

    feat/button-and-list-directives-implement

--- Prospect-Manager-Front/src/app/shared/directives/app-button.directive.ts
+++ Prospect-Manager-Front/src/app/shared/directives/app-button.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+type AppButtonType = 'primary' | 'secondary' | 'tertiary';
 
 @Directive({
   selector: '[appButton]'
 })
-export class AppButtonDirective {
+export class AppButtonDirective implements OnInit {
 
-  constructor() { }
+  @Input()
+  public conectaButton?: AppButtonType = 'primary';
+
+  @Input()
+  public ghost?: boolean;
+
+  constructor(
+    private el: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    this.applyStyles();
+  }
+
+  private applyStyles(): void {
+    this.el.nativeElement.className = (`app-button ${this.conectaButton || 'primary'} ${this.ghost ? 'ghost' : ''}`);
+  }
 
 }
