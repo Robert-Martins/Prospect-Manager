9af8fae6af31771fc4e3ecc01a6a56f6e239885c
Robert-Martins <robertcantares@gmail.com>
2023-09-29 23:44:51 -0300

    feat/prospect-form-init

--- Prospect-Manager-Front/src/app/views/prospects/prospects-form/prospects-form.component.ts
+++ Prospect-Manager-Front/src/app/views/prospects/prospects-form/prospects-form.component.ts
@@ -25,13 +25,12 @@ export class ProspectsFormComponent implements OnInit {
 
   public newProspect: boolean = true;
 
-  public prospectAnalysisStatus: EnumDto[] = [];
+  public naturalPerson: boolean = true;
 
   constructor(
     private toastr: ToastrService,
     private spinner: NgxSpinnerService,
     private prospectService: ProspectService,
-    private appService: AppService,
     private fb: FormBuilder,
     private router: Router,
     private activatedRoute: ActivatedRoute
@@ -45,24 +44,40 @@ export class ProspectsFormComponent implements OnInit {
     return this.prospectForm.value;
   }
 
+  public get personFormGroup(): FormGroup {
+    return this.prospectForm.get('person') as FormGroup;
+  }
+
+  public get companyFormGroup(): FormGroup {
+    return this.prospectForm.get('company') as FormGroup;
+  }
+
+  public get isNaturalPerson(): boolean {
+    return this.prospectFormValues.naturalPerson;
+  }
+
   public toList(): void {
     this.router.navigate(['/prospects']);
   }
 
-  private loadItems(): void {
-    this.loadStatus();
-    this.prospectId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.prospectId ?
-        this.read()
-      :
-        this.createProspectForm();
-  }
-
   public onSubmit(): void {
-    this.prospectId ?
+    this.prospectId && this.prospectId != '0' ?
         this.update()
       :
         this.create();
+  }
+
+  public onNaturalPersonChange(): void {
+    this.createProspectForm(this.prospectFormValues);
+  }
+
+  private loadItems(): void {
+    this.spinner.show()
+    this.prospectId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.prospectId && this.prospectId !== '0' ?
+        this.read()
+      :
+        this.createProspectForm();
   }
 
   public onDelete(): void {
@@ -89,12 +104,12 @@ export class ProspectsFormComponent implements OnInit {
 
   private read(): void {
     this.newProspect = false;
-    this.spinner.show();
     this.prospectService.read(this.prospectId)
       .subscribe(
         {
           next: prospect => {
             this.prospect = prospect;
+            this.naturalPerson = prospect?.naturalPerson;
             this.createProspectForm(prospect);
             this.spinner.hide();
           },
@@ -142,41 +157,34 @@ export class ProspectsFormComponent implements OnInit {
   }
 
   private createProspectForm(prospect: Prospect = new Prospect()): void {
+    this.spinner.hide();
     this.prospectForm = this.fb.group({
       id: [prospect?.id],
-      naturalPerson: [prospect?.naturalPerson, [Validators.required]],
-      person: prospect?.naturalPerson ? this.createPersonForm(prospect?.person) : null,
-      company: !prospect?.naturalPerson ? this.createCompanyForm(prospect?.company) : null
+      naturalPerson: [prospect?.naturalPerson != null ? prospect?.naturalPerson : true, [Validators.required]],
+      person: this.naturalPerson ? this.createPersonForm(prospect?.person) : null,
+      company: !this.naturalPerson ? this.createCompanyForm(prospect?.company) : null
     });
+    console.log(this.prospectForm)
   }
 
   private createCompanyForm(company: Company = new Company()): FormGroup {
     return this.fb.group({
       id: [company?.id],
-      companyName: [company?.companyName, [Validators.required]],
-      cnpj: [company?.cnpj, [Validators.required]],
-      mcc: [company.mcc, [Validators.required]],
-      contact: this.createPersonForm(company?.contact)
+      companyName: [company?.companyName, [Validators.required, Validators.maxLength(50)]],
+      cnpj: [company?.cnpj, [Validators.required, Validators.min(14), Validators.max(14)]],
+      mcc: [company?.mcc, [Validators.required, Validators.max(4)]],
+      contact: this.createPersonForm(company?.contact, true)
     });
   }
 
-  private createPersonForm(person: Person = new Person()): FormGroup {
+  private createPersonForm(person: Person = new Person(), isContact: boolean = false): FormGroup {
     return this.fb.group({
       id: [person?.id],
-      name: [person?.name,],
-      cpf: [person?.cpf],
-      email: [person?.email],
-      mcc: [person?.mcc]
+      name: [person?.name, [Validators.required, Validators.maxLength(50)]],
+      cpf: [person?.cpf, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      email: [person?.email, [Validators.required]],
+      mcc: [person?.mcc, isContact ? [ Validators.required, Validators.max(9999) ] : [Validators.max(9999)]]
     });
-  }
-
-  private loadStatus(): void {
-    this.appService.loadEnum('')
-      .subscribe(
-        {
-          next: enums => this.prospectAnalysisStatus = enums
-        }
-      );
   }
 
 }
--- Prospect-Manager-Front/src/app/views/prospects/prospects.module.ts
+++ Prospect-Manager-Front/src/app/views/prospects/prospects.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProspectsFormComponent } from './prospects-form/prospects-form.component';
 import { ProspectsListComponent } from './prospects-list/prospects-list.component';
 import { ProspectsRoutingModule } from './prospects-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
 
 
 
@@ -13,7 +19,14 @@ import { ProspectsRoutingModule } from './prospects-routing.module';
   ],
   imports: [
     CommonModule,
-    ProspectsRoutingModule
+    SharedModule,
+    ProspectsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatTableModule
   ]
 })
 export class ProspectsModule { }
